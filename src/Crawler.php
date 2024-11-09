2397fec5c0a36f6f198cfd1b4112fa872c09733f
phamusername <lanhp871@gmail.com>
2024-11-09 20:25:11 +0700

    update 1

--- src/Crawler.php
+++ src/Crawler.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Ophim\Crawler\OphimCrawler;
+namespace Ggg3\Subnhanhcrawler;
 
 use Ophim\Core\Models\Movie;
 use Illuminate\Support\Str;
@@ -10,7 +10,7 @@ use Ophim\Core\Models\Director;
 use Ophim\Core\Models\Episode;
 use Ophim\Core\Models\Region;
 use Ophim\Core\Models\Tag;
-use Ophim\Crawler\OphimCrawler\Contracts\BaseCrawler;
+use Ggg3\Subnhanhcrawler\Contracts\BaseCrawler;
 
 class Crawler extends BaseCrawler
 {
